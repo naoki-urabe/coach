a39a3cc28fba6bbe05a61bd9eecefd4807f75984
naoki-urabe <na0k1ura6e@gmail.com>
2021-04-30 15:47:37 +0900

    くじ引き画面追加

---
+++ front/src/components/Kuji.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{count}}</h1>
+  <button @click="kuji">くじびき</button>
+</template>
+
+<script>
+export default ({
+    data() {
+        return {count: 0}
+    },
+    methods: {
+        kuji:function() {
+            this.count++;
+        }
+    }
+})
+</script>

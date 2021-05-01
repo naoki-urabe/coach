b5cb69f021192391261c4328fd2d3d331bc30a6d
naoki-urabe <na0k1ura6e@gmail.com>
2021-05-01 20:29:10 +0900

    科目一覧取得

--- front/src/components/Register.vue
+++ front/src/components/Register.vue
@@ -6,6 +6,20 @@
     <br />
     <button @click="register">登録</button>
   </form>
+  <center>
+  <table>
+    <thead>
+      <th>科目名</th>
+      <th>科目コード</th>
+    </thead>
+    <tbody>
+      <tr v-for="(value, key) in subjects" :key="key">
+      <td>{{ value.subject }} </td>
+      <td>{{ value.subject_code }}</td>
+      </tr>
+    </tbody>
+  </table>
+  </center>
 </template>
 
 <script>
@@ -14,21 +28,26 @@ export default {
     return {
       subject: "",
       subjectCode: "",
+      subjects: []
     };
   },
   methods: {
     register: function () {
-      console.log("post")
-      console.log(this.subject);
-      console.log(this.subjectCode);
       const subject = {
         subject: this.subject,
-        subject_code: this.subjectCode
-      }
-      this.axios.post("http://localhost:8080/api/subject", subject).then(function (response) {
-        console.log(response);
-      });
+        subject_code: this.subjectCode,
+      };
+      this.axios
+        .post("http://localhost:8080/api/subject", subject)
+        .then(function (response) {
+          console.log(response);
+        });
     },
+  },
+  created: function () {
+    this.axios.get("http://localhost:8080/api/subject").then((res) => {
+      this.subjects = res.data;
+    });
   },
 };
 </script>

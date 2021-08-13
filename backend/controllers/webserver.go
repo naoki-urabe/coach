708d6553d2f47ff775ce4401aef5df7ea95a6ac8
naoki-urabe <na0k1ura6e@gmail.com>
2021-08-13 09:18:51 +0000

    user function分離

--- backend/controllers/webserver.go
+++ backend/controllers/webserver.go
@@ -1,43 +1,10 @@
 package controllers
 
 import (
-	"coach/models"
-	"crypto/rand"
-	"crypto/rsa"
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
-
-func registerUser(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w)
-	if (*r).Method == "OPTIONS" {
-		return
-	}
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var user models.User
-	if err := json.Unmarshal(reqBody, &user); err != nil {
-		log.Fatal(err)
-	}
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	publicKey := &privateKey.PublicKey
-	privateKeyPemStr := exportPrivateKeyAsPEMStr(privateKey)
-	publicKeyPemStr := exportPublicKeyAsPEMStr(publicKey)
-	user.PrivateKey = privateKeyPemStr
-	user.PublicKey = publicKeyPemStr
-	models.InsertUser(&user)
-	responseBody, err := json.Marshal(user)
-	if err != nil {
-		log.Fatal(err)
-	}
-	w.Write(responseBody)
-}
 
 func StartWebServer() error {
 	router := mux.NewRouter().StrictSlash(true)

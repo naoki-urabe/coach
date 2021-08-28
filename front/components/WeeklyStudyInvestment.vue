ba4233f42de4c1ad9be8b783737db4179f3ca4a3
naoki-urabe <na0k1ura6e@gmail.com>
2021-08-28 02:09:35 +0000

    グラフのモック追加

---
+++ front/components/WeeklyStudyInvestment.vue
@@ -0,0 +1,24 @@
+<script>
+import { Bar } from "vue-chartjs";
+export default {
+  extends: Bar,
+  data() {
+    return {
+      chartdata: {
+        labels: ["week1", "week2", "week3", "week4"],
+        datasets: [
+          {
+            label: ["週別学習投資量"],
+            backgroundColor: "#4169e1",
+            data: [140, 200, 300, 240],
+          },
+        ],
+      },
+      options: { responsive: true, maintainAspectRatio: false },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>

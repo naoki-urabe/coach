16496912d14729a365cc232b8ad665695b912a69
naoki-urabe <na0k1ura6e@gmail.com>
2021-08-28 02:09:35 +0000

    グラフのモック追加

---
+++ front/components/DailyStudyInvestment.vue
@@ -0,0 +1,35 @@
+<script>
+import { Bar } from "vue-chartjs";
+export default {
+  extends: Bar,
+  data() {
+    return {
+      chartdata: {
+        labels: [
+          "Monday",
+          "Tuesayn",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday",
+        ],
+        datasets: [
+          {
+            label: ["日別学習投資量"],
+            backgroundColor: "#4169e1",
+            data: [40, 30, 50, 40, 30, 20, 100],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <RecordLog />
+  <div>
+    <WeeklyStudyInvestment />
+    <DailyStudyInvestment />
+    <RecordLog />
+  </div>
 </template>
 
 <script>
-import RecordLog from "../components/RecordLog";
+import RecordLog from "@/components/RecordLog";
+import DailyStudyInvestment from "@/components/DailyStudyInvestment.vue";
+import WeeklyStudyInvestment from "@/components/WeeklyStudyInvestment.vue";
 export default {
-  components: { RecordLog },
+  components: { RecordLog, DailyStudyInvestment, WeeklyStudyInvestment },
 };
 </script>

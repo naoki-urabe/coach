bf7ddeace3e630abfb361c9ef9466ed5cc429102
naoki-urabe <na0k1ura6e@gmail.com>
2021-09-22 05:08:21 +0000

    微修正

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -26,7 +26,7 @@ export default {
     async login() {
       try {
         const response = await this.$auth.loginWith('local', {data: {"Id": this.id, "Pw": this.password}});
-        $auth.strategy.token.set(response.data)
+        this.$auth.strategy.token.set(response.data)
       } catch(error) {
         console.log(error)
       }

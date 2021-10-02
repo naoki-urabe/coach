8c1e1902a95f881438ef8849183d007bd9fc395c
naoki-urabe <na0k1ura6e@gmail.com>
2021-10-02 01:26:09 +0000

    自動デプロイできるよう設定

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+USER_HOST=`echo "$USER@$HOST"`
+SYNC_PATH=`echo "$USER_HOST:/home/$USER/app/coach"`
+SSH_PORT="ssh -p $HOST_PORT"
+rsync -e "$SSH_PORT" -acvz  --delete ./ $SYNC_PATH
+ssh $USER_HOST -p $HOST_PORT "cd app/coach;/usr/bin/docker-compose down -v; docker pull ghcr.io/naoki-urabe/coach/api:prod;docker pull ghcr.io/naoki-urabe/coach/front:prod;docker pull ghcr.io/naoki-urabe/coach/task:prod;/usr/bin/docker-compose -f docker-compose.prod.yml up -d"

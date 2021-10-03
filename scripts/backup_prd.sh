7e085744341b399e478dd17385a3fa04a1853571
naoki-urabe <na0k1ura6e@gmail.com>
2021-10-03 06:37:24 +0000

    fix shell

--- scripts/backup_prd.sh
+++ scripts/backup_prd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE=`date +"%Y-%m-%d"`
 USER=`whoami`
-docker exec -it coach_coach_db_prod_1 mysqldump coach_db > "/home/$USER/app/backups/coach/$DATE.sql"
+docker exec coach_coach_db_prod_1 mysqldump coach_db > "/home/$USER/app/backups/coach/$DATE.sql"

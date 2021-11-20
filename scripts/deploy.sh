108c97420627f68395dcf6ddc8d797d035be6ff0
naoki-urabe <na0k1ura6e@gmail.com>
2021-11-21 07:05:58 +0900

    不要なコンテナを削除するよう変更

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ USER_HOST=`echo "$USER@$HOST"`
 SYNC_PATH=`echo "$USER_HOST:/home/$USER/app/coach"`
 SSH_PORT="ssh -p $HOST_PORT"
 rsync -e "$SSH_PORT" -acvz  --delete ./ $SYNC_PATH
-ssh $USER_HOST -p $HOST_PORT "cd app/coach;/usr/bin/docker-compose down -v; docker pull ghcr.io/naoki-urabe/coach/api:prod;docker pull ghcr.io/naoki-urabe/coach/front:prod;docker pull ghcr.io/naoki-urabe/coach/task:prod;docker image prune -f;/usr/bin/docker-compose -f docker-compose.prod.yml up -d"
+ssh $USER_HOST -p $HOST_PORT "cd app/coach;/usr/bin/docker-compose down -v; docker pull ghcr.io/naoki-urabe/coach/api:prod;docker pull ghcr.io/naoki-urabe/coach/front:prod;docker pull ghcr.io/naoki-urabe/coach/task:prod;docker system prune -f;/usr/bin/docker-compose -f docker-compose.prod.yml up -d"
